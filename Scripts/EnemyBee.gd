9c8631de87859245e8fe394bb47fa969aaae5491
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2023-03-06 18:17:55 -0800

    Animated Bees!
    I animated the bees.

--- Scripts/EnemyBee.gd
+++ Scripts/EnemyBee.gd
@@ -1,9 +1,27 @@
 extends CharacterBody3D
 
+@export var frame1 : Node3D
+@export var frame2 : Node3D
 
+var animate_time = 50
+var counter : int = 0 
+var current_frame : int = 0 
+var number_of_frames : int = 2
+var frames = []
+
+func _ready():
+	frames = [frame1, frame2]
 
 func _physics_process(delta):
-	pass
+	animate_bee()
 
 func Kill_Me():
 	queue_free()
+
+func animate_bee():
+	counter = (counter + 1) % animate_time
+	#print_debug(counter)
+	current_frame = (counter / (animate_time/2)) % animate_time
+	#print_debug(current_frame)
+	frames[current_frame].show()
+	frames[(current_frame - 1) % number_of_frames].hide()
